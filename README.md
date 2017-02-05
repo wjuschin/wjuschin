6691ad0584ca9f47876958a92e8b46d3dc2c5dbf
Waldemar Juschin <wjuschin_iMac@Waldemars-iMac.local>
2017-02-05 15:02:27 -0800

    Added all files for the game.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# wjuschin
+# Frogger Arcade Game aka. Free Tha Carter V
+
+## Summary
+Frogger Arcade Game aka. Free Tha Carter V is an arcade game where you have to try to steal back Tha Carter V from Martin Shkreli.
+
+### Goal
+The goal in this game is to cross to the other side and snag Tha Carter V from Martin Shkreli away. But be careful, Martin is fast and wants his album back. How many times can you get it away from him? If he catches you, you will start back at zero.
+
+### Controls
+Lil Wayne moves up/right/left/down with the arrow keys on the keyboard.
+
+### Installation
+
+1. Download WJgame.zip
+2. Unzip the file
+3. Open index.html, it will open your browser and the game will get started.
---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Enemies our player must avoid
+var Enemy = function(row, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    
+    this.x = -100;
+    this.y = 55 + (row - 1) * 80;
+    this.speed = speed;
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug2.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + this.speed * dt;
+    if (this.x > 480) this.x = -90;
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function() {
+
+    this.x = 200;
+    this.y = 380;
+    this.score = 0;
+
+    this.sprite = 'images/char-wayne.png';
+
+};
+
+Player.prototype.update = function() {
+    this.x = 200;
+    this.y = 380;
+    this.score = this.score + 1;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) { 
+
+    if ( key === 'left' ) {
+        if ( this.x > 0 ) {
+            this.x = this.x - 100;
+        }
+    } else if ( key === 'right' ) {
+        if ( this.x < 400 ) {
+            this.x = this.x + 100;
+        }
+    } else if ( key === 'up' ) {
+        if ( this.y > 0 ) {
+            this.y = this.y - 80;
+        }
+    } else if ( key === 'down') {
+        if ( this.y < 380 ) {
+            this.y = this.y + 80;
+        } 
+    }
+};
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 380;
+    this.score = 0;    
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var allEnemies = [];
+
+for (var i = 0; i < 7; i++) {
+    var random_speed = getRandomNumber(10, 31) * 10;
+    var random_row = getRandomNumber(1, 4);
+    allEnemies[i] = new Enemy(random_row, random_speed);
+}
+
+var player = new Player();
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
